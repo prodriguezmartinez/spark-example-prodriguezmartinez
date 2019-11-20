e12d6f84203966518234d0b320da540b6f779a2b
prodriguezmartinez <p.rodriguezmartin@alumnos.urjc.es>
2019-11-20 12:09:59 +0100

    inicializando

---
+++ src/main/java/urjc/isi/pruebasSparkJava/App.java
@@ -0,0 +1,13 @@
+package urjc.isi.pruebasSparkJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
